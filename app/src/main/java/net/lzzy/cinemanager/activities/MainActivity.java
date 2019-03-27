92becb56c71b95e0f17b2af93ff3cd9796b5007b
Administrator <1840383012@qq.com>
2019-03-27 09:15:36 +0800

    托管代码重构 （3月27日 第2节）

--- app/src/main/java/net/lzzy/cinemanager/activities/MainActivity.java
+++ app/src/main/java/net/lzzy/cinemanager/activities/MainActivity.java
@@ -1,9 +1,12 @@
 package net.lzzy.cinemanager.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.util.SparseArray;
 import android.view.View;
 import android.view.Window;
 import android.widget.LinearLayout;
@@ -11,6 +14,8 @@ import android.widget.SearchView;
 import android.widget.TextView;
 
 import net.lzzy.cinemanager.R;
+import net.lzzy.cinemanager.fragments.AddCinemasFragment;
+import net.lzzy.cinemanager.fragments.AddOrdersFragment;
 import net.lzzy.cinemanager.fragments.CinemasFragment;
 import net.lzzy.cinemanager.fragments.OrdersFragment;
 
@@ -22,6 +27,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private TextView tvTitle;
     private SearchView search;
     private FragmentManager manager=getSupportFragmentManager();
+    private SparseArray<String> titleArray=new SparseArray<>();
+    private SparseArray<Fragment> fragmentArray=new SparseArray<>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,6 +39,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     public void setTitleMenu(){
+        titleArray.put(R.id.bar_title_tv_add_cinema,"添加影院");
+        titleArray.put(R.id.bar_title_tv_add_order,"添加订单");
+        titleArray.put(R.id.bar_title_tv_view_cinema,"查看影院");
+        titleArray.put(R.id.bar_title_tv_view_order,"我的订单");
         layoutMenu=findViewById(R.id.bar_title_layout_menu);
         layoutMenu.setVisibility(View.GONE);
         findViewById(R.id.bar_title_img_menu).setOnClickListener(v ->{
@@ -52,28 +64,45 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onClick(View v) {
         layoutMenu.setVisibility(View.GONE);
-        switch (v.getId()) {
+        tvTitle.setText(titleArray.get(v.getId()));
+        Fragment fragment=fragmentArray.get(v.getId());
+        FragmentTransaction transaction=manager.beginTransaction();
+        if (fragment==null){
+            fragment=createFragment(v.getId());
+            fragmentArray.put(v.getId(),fragment);
+            transaction.add(R.id.relativ_fragments,fragment);
+        }
+
+        //把所有的fragment隐藏
+        for (Fragment f:manager.getFragments()){
+            transaction.hide(f);
+        }
+        transaction.show(fragment).commit();
+
+
+    }
+
+    private Fragment createFragment(int id) {
+        switch (id) {
             case R.id.bar_title_tv_add_cinema:
-                tvTitle.setText(R.string.bar_title_menu_add_cinema);
-                break;
+                return new AddCinemasFragment();
             case R.id.bar_title_tv_add_order:
-                tvTitle.setText(R.string.bar_title_menu_add_order);
-                break;
+                return new AddOrdersFragment();
             case R.id.bar_title_tv_view_cinema:
-                tvTitle.setText(R.string.bar_title_menu_view_cinema);
-                manager.beginTransaction()
-                        .replace(R.id.relativ_fragments,new CinemasFragment())
-                        .commit();
-                break;
+//                manager.beginTransaction()
+//                        .replace(R.id.relativ_fragments,new CinemasFragment())
+//                        .commit();
+                return new CinemasFragment();
             case R.id.bar_title_tv_view_order:
-                tvTitle.setText(R.string.bar_title_menu_order);
-                manager.beginTransaction()
-                        .replace(R.id.relativ_fragments,new OrdersFragment())
-                        .commit();
-                break;
+//                manager.beginTransaction()
+//                        .replace(R.id.relativ_fragments,new OrdersFragment())
+//                        .commit();
+                return new OrdersFragment();
             default:
                 break;
         }
-
+        return null;
     }
+
+
 }
